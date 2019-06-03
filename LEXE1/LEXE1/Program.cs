f115a1914d16ffa75c1b1f14b79f148f9ea243a1
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-06-03 14:36:35 +0200

    Add files via upload

---
+++ LEXE1/LEXE1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LEXE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter diffrent names: ");
+               var name = Console.ReadLine();
+                if (!String.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("A sertan amount of names have been Entered ");
+                    continue;
+                }
+                break;
+               
+            }
+        }
+    }
+}
